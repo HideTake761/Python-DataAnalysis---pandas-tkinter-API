7e3039eb5aa595d6d91512b39bda65f7c3825061
HideTake761 <55819922+HideTake761@users.noreply.github.com>
2025-09-10 23:52:37 +0900

    Update mergePDF.py

--- mergePDF.py
+++ mergePDF.py
@@ -2,8 +2,11 @@ import PyPDF2
 
 merger = PyPDF2.PdfMerger() # PyPDF2ライブラリのPdfMerger()クラスからインスタンスを作る
 
-merger.append('/Users/taken/Desktop/sample01.pdf')
-merger.append('/Users/taken/Desktop/sample02.pdf')
+# 統合したいファイルとその置き場所
+merger.append('/Users/(ユーザー名)/Desktop/sample01.pdf')
+merger.append('/Users/(ユーザー名)/Desktop/sample02.pdf')
 
-merger.write('/Users/taken/Desktop/sample_merged.pdf')
+# 統合したファイルのファイル名とその置き場所
+merger.write('/Users/(ユーザー名)/Desktop/sample_merged.pdf')
+
 merger.close()
