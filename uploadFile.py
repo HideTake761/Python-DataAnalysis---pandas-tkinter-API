b4dce98c24cc9a33ae3476c4b9aafe3c2e65ea83
HideTake761 <55819922+HideTake761@users.noreply.github.com>
2025-09-11 00:01:24 +0900

    Update uploadFile.py

--- uploadFile.py
+++ uploadFile.py
@@ -2,15 +2,21 @@
  
 import paramiko
  
-HOST = '192.168.56.102'
-USER = 'user'
-PWD = '1234'
+HOST = 'ファイルをアップロードしたいサーバのIPアドレス'
+USER = 'user' # サーバにアクセスする際のユーザー名
+PWD = 'password' # パスワード
  
-LOCAL_PATH  = "/Users/taken/Desktop/sample_merge.pdf"
+LOCAL_PATH  = "/Users/(ユーザー名)/Desktop/sample_merge.pdf"
 REMOTE_PATH = "/home/user/sample_merge.pdf"
  
-ssh = paramiko.SSHClient()
+ssh = paramiko.SSHClient() # paramikoライブラリのSSHClientクラスのインスタンスを作成
 ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+# set_missing_host_key_policy():未知のホストキーに対する動作を設定するメソッド
+# AutoAddPolicy():未知のホストキーに対するSSHClient()のポリシー。初めてアクセスするサーバ
+# でもホストキーを自動的に追加し、次回以降の接続に利用する。ポリシーはほかには
+# RejectPolicy():デフォルト設定。不明なホスト名とキーを自動的に拒否
+# WarningPolicy():警告をログに記録してから接続を行う
+# paramikoのドキュメント https://docs.paramiko.org/en/stable/api/client.html
 ssh.connect(HOST, username=USER, password=PWD)
  
 sftp = ssh.open_sftp()
